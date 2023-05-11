9ee2a392083534a5ecf1c380d629fb428e026771
Oyebode Adetunji <adetunji.oyebode6@gmail.com>
2023-05-11 15:11:45 +0000

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a function that queries the Reddit API
+    and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyApp/1.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        count = data.get('data').get('subscribers')
+        return count
